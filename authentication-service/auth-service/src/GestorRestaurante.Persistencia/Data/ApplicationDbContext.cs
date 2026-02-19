5e09b558b0fd0e822c9ab82ec31c5feb7080e71a
jgonzales-2024075 <jgonzales-2024075@kinal.edu.gt>
2026-02-18 22:00:18 -0600

    fix(RBK-005): Se corrigieron archivos y repositorios en la capa Persistencia

--- authentication-service/auth-service/src/GestorRestaurante.Persistencia/Data/ApplicationDbContext.cs
+++ authentication-service/auth-service/src/GestorRestaurante.Persistencia/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
 using AuthService.Domain.Entities;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Persistence.Context
 {
@@ -10,13 +10,15 @@ namespace AuthService.Persistence.Context
         {
         }
 
-
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<Rol> Roles { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>().ToTable("usuarios");
+            modelBuilder.Entity<Rol>().ToTable("roles");
 
             // Relación Usuarios -> Roles
             modelBuilder.Entity<Usuario>()
@@ -25,19 +27,23 @@ namespace AuthService.Persistence.Context
                 .HasForeignKey(u => u.IdRol)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            
             modelBuilder.Entity<Usuario>(entity =>
             {
                 entity.HasKey(u => u.IdUsuario);
+
+                entity.Property(u => u.Nombre).IsRequired().HasMaxLength(100);
                 entity.Property(u => u.Correo).IsRequired().HasMaxLength(150);
-                entity.HasIndex(u => u.Correo).IsUnique(); 
+                entity.Property(u => u.Contrasena).IsRequired().HasMaxLength(255);
+                entity.Property(u => u.Estado).HasDefaultValue(true);
+
+                entity.HasIndex(u => u.Correo).IsUnique();
             });
 
-            
             modelBuilder.Entity<Rol>(entity =>
             {
                 entity.HasKey(r => r.IdRol);
                 entity.Property(r => r.Nombre).IsRequired().HasMaxLength(50);
+                entity.HasIndex(r => r.Nombre).IsUnique();
             });
         }
     }
